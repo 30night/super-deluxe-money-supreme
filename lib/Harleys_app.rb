5e3ee40b916413c68fb6fc1bac2571077e5eb68c
30night <haroscmil2011@gmail.com>
2020-03-13 13:23:20 +1100

    started on menu class

--- lib/Harleys_app.rb
+++ lib/Harleys_app.rb
@@ -12,3 +12,19 @@ def welcome
   text = Artii::Base.new :font => 'doom'
   puts text.asciify('Money').colorize(:color => :blue, :background => :yellow)
 end
+
+class Menu
+  def self.view_table(table_name)
+    puts table_name
+  end
+  def self.update_table(table_name)
+  puts  'day'
+  day = gets.chomp.to_i
+  puts 'how how much did you earn today?'
+  assets = gets.chomp.to_i
+  puts 'how much did you spend today?'
+  liabilities = gets.chomp.to_i
+  capital = assets - liabilities
+  table_name << [day,assets,liabilities,capital]
+  end
+  end
