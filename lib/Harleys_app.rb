52ec4b1ad0bab4f564a7162827f67d7e6195c86d
30night <haroscmil2011@gmail.com>
2020-03-13 13:20:16 +1100

    added the requires and def welcome

--- lib/Harleys_app.rb
+++ lib/Harleys_app.rb
@@ -1,6 +1,14 @@
 require "Harleys_app/version"
+require 'tty-table'
+require 'csv'
+require 'colorize'
+require 'artii'
+require 'colorized_string'
+# require "tty-prompt"
 
-module HarleysApp
-  class Error < StandardError; end
-  puts 'hello world'
+# prompt = TTY::Prompt.new
+
+def welcome
+  text = Artii::Base.new :font => 'doom'
+  puts text.asciify('Money').colorize(:color => :blue, :background => :yellow)
 end
