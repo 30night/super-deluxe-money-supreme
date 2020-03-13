b2a1ebd5aac5b5ad102b0dc42458eb2396c4367e
30night <haroscmil2011@gmail.com>
2020-03-13 13:24:06 +1100

    finished menu calss

--- lib/Harleys_app.rb
+++ lib/Harleys_app.rb
@@ -27,4 +27,19 @@ class Menu
   capital = assets - liabilities
   table_name << [day,assets,liabilities,capital]
   end
+  def self.remove_capital
+    p 'how much capital would you like to remove'
+    capital_removal = gets.chomp
+  end
+  def self.quit(user_name, table)
+    Menu.save(user_name, table)
+    exit
+  end
+  def self.save(user_name,table)
+    CSV.open(user_name + '.csv', 'w') do |file|
+      table.each do |row|
+        file << row 
+      end
+    end
+  end
   end
